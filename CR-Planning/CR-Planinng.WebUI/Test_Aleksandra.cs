b5e387d1d004b3a2fb9d735ae80bfc5b3a8582e8
Aleksandra <aleksanda.czerwa@gmail.com>
2017-04-12 21:21:01 +0100

    My test class.

---
+++ CR-Planning/CR-Planinng.WebUI/Test_Aleksandra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CR_Planinng.WebUI
+{
+    public class Test_Aleksandra
+    {
+        // There is the test code. Imagine that.
+    }
+}
