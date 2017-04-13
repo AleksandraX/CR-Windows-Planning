ac3d58aa24c2e893a16503a0638fd4acb927d6da
pawpiek <pawelpiekarski7@gmail.com>
2017-04-13 15:48:39 +0200

    Test_Pawel push.

---
+++ CR-Planning/CR-Planinng.WebUI/Test_Pawel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CR_Planinng.WebUI
+{
+    public class Test_Pawel
+    {
+        // test class
+    }
+}
